552bb2217c3465e9e251432a2e27c23e1231cbbb
Alexander Hjorth <ahjorth@gmail.com>
2015-10-23 16:45:43 +0200

    move api key to dotenv

--- config/steam-api.php
+++ config/steam-api.php
@@ -6,5 +6,5 @@ return array(
 	 * You can get a steam API key from http://steamcommunity.com/dev/apikey
 	 * Once you get your key, add it here.
 	*/
-	'steamApiKey' => '83EAFD86CEBD5E6A8337012D6851DFCA',
+	'steamApiKey' => env('STEAM_API_KEY'),
 );
